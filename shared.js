91db80a28a63fb51debbe3d0d3057fe001e403fb
Filipe-vilela-felix <fvf0092000@gmail.com>
2024-12-04 16:23:12 -0300

    section 9: opening and closing the hamburger menu

--- shared.js
+++ shared.js
@@ -1,6 +1,9 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(background)
 for (var i = 0; i < selectPlanButtons.length; i++) {
@@ -9,3 +12,20 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
         backdrop.style.display = 'block';
     })
 }
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
